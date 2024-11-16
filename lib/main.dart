b39adae11b6998d8ce7e049676f199738784a530
erfan <ehoseyni1383@gmail.com>
2024-11-16 18:25:04 +0330

    u

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'dart:ui';
 
 import 'package:GreenWave/Core/Constants/app_route.dart';
 import 'package:GreenWave/Features/Intro/IntroSplash/intro_splash_view.dart';
+import 'package:GreenWave/Features/Intro/IntroWelcome/intro_welcome_view.dart';
 import 'package:GreenWave/Features/MainWrapper/MainWrapperBottomNav/main_wrapper_bottom_nav_view.dart';
 import 'package:GreenWave/Features/MainWrapper/MainWrapperProfile/main_wrapper_profile_view.dart';
 import 'package:GreenWave/Features/MainWrapper/MainWrapperWallet/main_wrapper_wallet_view.dart';
@@ -14,9 +15,7 @@ import 'Core/Themes/themes.dart';
 
 //! Main function to initialize the app and set up system configurations
 void main() async {
-  WidgetsFlutterBinding
-      .ensureInitialized(); //* Ensure Flutter is fully initialized before running
-  runApp(const Main()); //* Run the main app widget
+ runApp(const Main()); //* Run the main app widget
 }
 
 //! Main class for the app, extends StatelessWidget
@@ -34,7 +33,7 @@ class Main extends GetView<TelegramController> {
       initialBinding: MyBindings(),
 
       getPages: AppRoute.pages,
-      home: const IntroSplashView(),
+      home: const IntroWelcomeView(),
     );
   }
 }
